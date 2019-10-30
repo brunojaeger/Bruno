72109be15c254e43c96cb12ac9982bd17269e4b3
brunojaeger <56429802+brunojaeger@users.noreply.github.com>
2019-10-30 15:31:34 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
+
+function update() {
+  var element = document.getElementById("myprogressBar");
+  var width = 1;
+  var identity = setInterval(scene, 10);
+  function scene() {
+    if (width >= 100) {
+      clearInterval(identity);
+    } else {
+      width++;
+      element.style.width = width + '%';
+      element.innerHTML = width * 1  + '%';
     }
   }
 }
