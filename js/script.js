df109ef184f55196fea3184708e610407e5aebc4
brunojaeger <56429802+brunojaeger@users.noreply.github.com>
2019-10-30 14:55:02 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-window.onscroll = function () {
-    myFunction()
-};
-var header = document.getElementById("header");
-var sticky = nav.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        nav.classList.add("sticky");
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
     }
-    else {
-        nav.classList.remove("sticky");
-    }
+  }
 }
